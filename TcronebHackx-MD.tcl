b24208be6add564ad920f704dd3ffbf86a8da192
Tcroneb Hackx <cartoonboyzfamily@gmail.com>
2024-10-02 21:35:16 +0200

    Create TcronebHackx-MD.tcl

---
+++ TcronebHackx-MD.tcl
@@ -0,0 +1,31 @@
+#!/usr/bin/env expect -f
+
+
+# sets the variables
+set fname "Tcroneb"
+set linux "Artix"
+set plang "python"
+
+
+# Set echo to off
+stty -echo
+
+# Send message to user
+send "Enter any key to start: "
+
+# Expect everything from user
+expect -re "(.*)\n"
+
+# Send new line to Terminal
+send "\n"
+
+# Set echo to on
+stty echo
+
+# Prints variables
+puts "Hi my name is $fname"
+puts "I'm a linux user, using $linux now"
+puts "I'm a $plang developer"
+
+# Exit
+exit 1
